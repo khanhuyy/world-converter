d40c34d453e9794289dc22b8f302b48ea70865b9
khanhuy <104879316+khanhuyy@users.noreply.github.com>
2024-10-11 18:00:30 +0700

    Update numtotext.go

--- numtotext/vi/numtotext.go
+++ numtotext/vi/numtotext.go
@@ -25,35 +25,30 @@ func ToWords(n int) string {
 		9: "tỉ",
 	}
 
-	num := map[string]string{
-		"0": "không",
-		"1": "một",
-		"2": "hai",
-		"3": "ba",
-		"4": "bốn",
-		"5": "năm",
-		"6": "sáu",
-		"7": "bảy",
-		"8": "tám",
-		"9": "chín",
+	num := map[rune]string{
+		'0': "không",
+		'1': "một",
+		'2': "hai",
+		'3': "ba",
+		'4': "bốn",
+		'5': "năm",
+		'6': "sáu",
+		'7': "bảy",
+		'8': "tám",
+		'9': "chín",
 	}
 	s := fmt.Sprintf("%d", n)
-	var lenString = len(s)
-	check9 := lenString - (lenString % 9)
-	check3 := lenString - (lenString % 3) // position multi tripple (kiem tra bo 3)
-	offset := lenString - check3 + 1      // phan le
-	oneFlag := false                      // flag to check exceptionOne
+	var result string
+	var lenString = len(s) // flag to check exceptionOne
 	if lenString < 2 {
-		result := num[s]
-		charResult := []byte(result)
-		charResult[0] = byte(unicode.ToUpper(rune(charResult[0])))
-		result = string(charResult)
-		result = fmt.Sprintf(strings.TrimRight(result, " "))
-		result += " đồng"
-		return result
-	} else {
-		result := ""
-		var flag int64 // kiem tra so khong
+		result = num[rune(s[0])]
+	} else { // len s = 5
+		check9 := lenString - (lenString % 9) // 5
+		check3 := lenString - (lenString % 3) // 2
+		offset := lenString - check3 + 1      // 4
+		oneFlag := false
+
+		var zeroFlag int64 // continuous zero
 		for pos, char := range s {
 			if offset == 1 {
 				offset = 3
@@ -62,95 +57,95 @@ func ToWords(n int) string {
 				offset -= 1
 			}
 			unit := len(s) - pos - 1
-			triple := unit % 3
-			switch triple {
+			set3 := unit % 3
+			switch set3 {
 			case 1:
-				if fmt.Sprintf("%c", char) == "0" {
-					flag += 1
-				} else if fmt.Sprintf("%c", char) == "1" {
-					if flag%3 == 1 {
-						result += num["0"] + " " + group[2] + " " + exception[10] + " "
+				if char == '0' {
+					zeroFlag += 1
+				} else if char == '1' {
+					if zeroFlag%3 == 1 {
+						result += num['0'] + " " + group[2] + " " + exception[10] + " "
 					} else {
 						result += exception[10] + " "
 					}
-					flag = 0
+					zeroFlag = 0
 				} else {
-					if flag%3 == 1 {
-						result += num["0"] + " " + group[2] + " " + num[fmt.Sprintf("%c", char)] + " " + group[1] + " "
+					if zeroFlag%3 == 1 {
+						result += num['0'] + " " + group[2] + " " + num[char] + " " + group[1] + " "
 					} else {
-						result += num[fmt.Sprintf("%c", char)] + " " + group[1] + " "
+						result += num[char] + " " + group[1] + " "
 					}
 					oneFlag = true
-					flag = 0
+					zeroFlag = 0
 				}
 			case 2:
-				if fmt.Sprintf("%c", char) == "0" {
-					flag += 1
+				if char == '0' {
+					zeroFlag += 1
 				} else {
-					flag = 0
-					result += num[fmt.Sprintf("%c", char)] + " " + group[2] + " "
+					zeroFlag = 0
+					result += num[char] + " " + group[2] + " "
 				}
 			default:
-				if fmt.Sprintf("%c", char) == "0" {
-					flag += 1
-				} else if fmt.Sprintf("%c", char) == "1" {
-					if flag > 1 {
-						result += exception[0] + " " + num["1"] + " "
+				if char == '0' {
+					zeroFlag += 1
+				} else if char == '1' {
+					if zeroFlag > 1 {
+						result += exception[0] + " " + num['1'] + " "
 						oneFlag = false
 					} else {
 						if oneFlag {
 							result += exception[1] + " "
 							oneFlag = true
 						} else {
-							result += num["1"] + " "
+							result += num['1'] + " "
 						}
 					}
-					flag = 0
-				} else if fmt.Sprintf("%c", char) == "4" {
-					if flag > 1 {
+					zeroFlag = 0
+				} else if char == '4' {
+					if zeroFlag > 1 {
 						result += exception[0] + " " + exception[4] + " "
 					} else {
-						if pos != 0 {
+						if pos == 0 {
+							println(pos)
 							result += exception[4] + " "
 						} else {
-							result += num["4"] + " "
+							result += num['4'] + " "
 						}
 					}
-					flag = 0
-				} else if fmt.Sprintf("%c", char) == "5" {
-					if flag > 1 {
-						result += exception[0] + " " + num["5"] + " "
+					zeroFlag = 0
+				} else if char == '5' {
+					if zeroFlag > 1 {
+						result += exception[0] + " " + num['5'] + " "
 					} else {
 						result += exception[5] + " "
 					}
-					flag = 0
+					zeroFlag = 0
 				} else {
-					if flag > 1 {
-						result += exception[0] + " " + num[fmt.Sprintf("%c", char)] + " "
+					if zeroFlag > 1 {
+						result += exception[0] + " " + num[char] + " "
 					} else {
-						result += num[fmt.Sprintf("%c", char)] + " "
+						result += num[char] + " "
 					}
-					flag = 0
+					zeroFlag = 0
 				}
 				if check3%9 == 0 && check3 > 0 {
 					check9 -= 9
 					result += group[9] + " "
 				} else {
-					if flag < 1 {
+					if zeroFlag < 1 {
 						result += group[check3%9] + " "
-						flag = 0
-					} else if flag/3 < 1 {
+						zeroFlag = 0
+					} else if zeroFlag/3 < 1 {
 						result += group[check3%9] + " "
-						flag = 0
+						zeroFlag = 0
 					}
 				}
 			}
 		}
-		charResult := []byte(result)
-		charResult[0] = byte(unicode.ToUpper(rune(charResult[0])))
-		result = string(charResult)
-		result = fmt.Sprintf(strings.TrimRight(result, " "))
-		result += " đồng"
-		return result
 	}
+	charResult := []byte(result)
+	charResult[0] = byte(unicode.ToUpper(rune(charResult[0])))
+	result = string(charResult)
+	result = fmt.Sprintf(strings.TrimRight(result, " "))
+	return result
 }
